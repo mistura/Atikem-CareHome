621e4702728f0eecec317a12cefc96e55fb9ced8
Mistura <quadri.mistura@gmail.com>
2023-03-30 15:00:40 +0100

    firstversion

---
+++ src/routes.jsx
@@ -0,0 +1,44 @@
+import { Home, Profile, SignIn, SignUp } from "@/pages";
+import {
+  HomeIcon,
+  UserCircleIcon,
+  ArrowRightOnRectangleIcon,
+  UserPlusIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/solid";
+
+export const routes = [
+  {
+    icon: HomeIcon,
+    name: "About Us",
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    icon: UserCircleIcon,
+    name: "Contact",
+    path: "/home",
+    element: <Profile />,
+  },
+  {
+    icon: ArrowRightOnRectangleIcon,
+    name: "Services",
+    path: "/home",
+    element: <SignIn />,
+  },
+  {
+    icon: UserPlusIcon,
+    name: "Blog",
+    path: "/home",
+    element: <SignUp />,
+  },
+  // {
+  //   icon: DocumentTextIcon,
+  //   name: "Docs",
+  //   href: "https://www.material-tailwind.com/docs/react/installation",
+  //   target: "_blank",
+  //   element: "",
+  // },
+];
+
+export default routes;
